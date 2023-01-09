2c809e508f62deef24378bc1f6830378c92a5e63
arkmadj <ahmadjinadu@gmail.com>
2023-01-09 23:32:16 +0100

    Added header component

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
 	return (
 		<header className="p-5 bg-blue-500">
-			<p>Header</p>
+			<p className="font-bold text-white">Header</p>
 		</header>
 	);
 }
