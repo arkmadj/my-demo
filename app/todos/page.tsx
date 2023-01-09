587b17bff1fcc8cc1da881d15e1681321ac32f19
arkmadj <ahmadjinadu@gmail.com>
2023-01-09 23:46:35 +0100

    Added todo list component

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Todo } from "../../typings";
-
-const fetchTodos = async () => {
-	const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
-	const todos: Todo[] = await res.json();
-	return todos;
-};
+import TodosList from "./TodoList";
 
 function Todos() {
-	return <div>Todos</div>;
+	return (
+		<div>
+			<TodosList />
+		</div>
+	);
 }
 
 export default Todos;
