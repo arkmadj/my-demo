495052264913a2bb895a6ee33ce1474086753350
arkmadj <ahmadjinadu@gmail.com>
2023-01-10 22:10:07 +0100

    Trimmed todos

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -38,6 +38,8 @@ export async function generateStaticParams() {
 	const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
 	const todos: Todo[] = await res.json();
 
+	const trimmedTodos = todos.splice(0, 10);
+
 	return todos.map((todo) => ({
 		todoId: todo.id.toString(),
 	}));
