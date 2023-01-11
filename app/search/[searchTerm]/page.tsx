5c55285f5f385a2bc91437bf0f7667e892931bf5
arkmadj <ahmadjinadu@gmail.com>
2023-01-11 22:13:12 +0100

    Added search function

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -10,6 +10,9 @@ const search = async (searchTerm: string) => {
 	const res = await fetch(
 		`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
 	);
+
+	const data = await res.json();
+	return data;
 };
 
 function SearchResults({ params: { searchTerm } }: PageProps) {
