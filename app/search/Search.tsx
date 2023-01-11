5acd8b2fe5929006fe2c898da21e8c9c57de4c6e
arkmadj <ahmadjinadu@gmail.com>
2023-01-11 21:36:25 +0100

    Added state

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,6 +1,10 @@
 'use client'
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
 
 function Search() {
+  const [search, setSearch] = useState("")
+  const router = useRouter()
   return (
     <div>I am a Search component</div>
   )
