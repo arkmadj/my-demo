179d27365e14e62db8015b541d4ded9d8d02e4dc
arkmadj <ahmadjinadu@gmail.com>
2023-01-11 22:20:40 +0100

    Rendered search results

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -22,13 +22,26 @@ const search = async (searchTerm: string) => {
 		`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
 	);
 
-	const data = await res.json();
+	const data: SearchResult = await res.json();
 	return data;
 };
 
 async function SearchResults({ params: { searchTerm } }: PageProps) {
 	const searchResults = await search(searchTerm);
-	return <div>SearchResults</div>;
+
+	return (
+		<div>
+			<p className="text-sm text-gray-500">You searched for: {searchResults}</p>
+			<ol className="p-5 space-y-5">
+				{searchResults.organic_results.map((result) => (
+					<li key={result.position} className="list-decimal">
+						<p className="font-bold">{result.title}</p>
+						<p>{result.snippet}</p>
+					</li>
+				))}
+			</ol>
+		</div>
+	);
 }
 
 export default SearchResults;
