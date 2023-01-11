6fa3ab9cccad811e964e6c0ef5cb84356e79110e
arkmadj <ahmadjinadu@gmail.com>
2023-01-11 21:38:19 +0100

    Added handle search function

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,13 +1,17 @@
-'use client'
-import { useRouter } from "next/navigation"
-import React, { useState } from "react"
+"use client";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 function Search() {
-  const [search, setSearch] = useState("")
-  const router = useRouter()
-  return (
-    <div>I am a Search component</div>
-  )
+	const [search, setSearch] = useState("");
+	const router = useRouter();
+
+	const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setSearch("");
+		router.push(`/search/${search}`);
+	};
+	return <div>I am a Search component</div>;
 }
 
-export default Search
+export default Search;
